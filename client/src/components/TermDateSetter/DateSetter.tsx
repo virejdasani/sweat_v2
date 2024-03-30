b63dbe349d12daa165278b145b9d2cc40ad0a728
VirejDasani <dasanivirej@gmail.com>
2024-03-30 19:57:29 +0000

    chore: add todos

--- client/src/components/TermDateSetter/DateSetter.tsx
+++ client/src/components/TermDateSetter/DateSetter.tsx
@@ -39,6 +39,7 @@ const eventsOnCalendar: Event[] = [
   },
 ];
 
+// TODO: make input take input like 'week 1 thursday' and auto populate the date
 // TODO: fix styling (spacing of input form fields and buttons)
 // TODO: add data to centralised database
 // TODO: put show modal element in its own component so it can be rendered here and professor calendar view too

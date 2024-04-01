65b95ec8420895c8e169b4e39e518ca9e7302a29
VirejDasani <dasanivirej@gmail.com>
2024-04-01 19:18:51 +0100

    fix: margins, padding, other style elements

--- client/src/components/TermDateSetter/DateSetter.tsx
+++ client/src/components/TermDateSetter/DateSetter.tsx
@@ -39,7 +39,6 @@ const eventsOnCalendar: Event[] = [
   },
 ];
 
-// TODO: fix styling (spacing of input form fields and buttons)
 // TODO: add data to centralised database
 // TODO: put show modal element in its own component so it can be rendered here and professor calendar view too
 // TODO: change from DatePicker to mui date picker if that looks better (tried mui, the documentation is terrible and not worth it)
@@ -398,7 +397,6 @@ function DateSetter() {
                   </div>
                 </div>
                 <div className="modal-footer">
-                  {/* new add to calendar button but this one dynamically gets date and event name */}
                   <AddToCalendarButton
                     name={eventTitle + ' (Added from SWEAT)'}
                     options={['Apple', 'Google', 'Outlook.com']}

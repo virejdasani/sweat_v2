9a97b54bca3b08a9512cd5bb5bb5ccfa23c465c4
VirejDasani <dasanivirej@gmail.com>
2024-04-06 18:40:14 +0100

    feat: add calendar schema for mongodb

--- server/src/models/calendar.js
+++ server/src/models/calendar.js
@@ -1,1 +1,28 @@
-console.log('calendar.js');
+const mongoose = require('mongoose');
+
+const calendarSchema = new mongoose.Schema({
+  events: [eventSchema],
+});
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+  allDay: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const Calendar = mongoose.model('Calendar', calendarSchema);
+
+module.exports = Calendar;

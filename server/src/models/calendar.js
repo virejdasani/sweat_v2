d8d7e1ba8d594f915b6a23fcdb896bd50e301795
VirejDasani <dasanivirej@gmail.com>
2024-04-09 00:05:09 +0100

    fix: calendar schema is correct now

--- server/src/models/calendar.js
+++ server/src/models/calendar.js
@@ -7,29 +7,6 @@ const calendarSchema = new mongoose.Schema({
   end: Date,
 });
 
-// const eventSchema = new mongoose.Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   start: {
-//     type: Date,
-//     required: true,
-//   },
-//   end: {
-//     type: Date,
-//     required: true,
-//   },
-//   allDay: {
-//     type: Boolean,
-//     default: true,
-//   },
-// });
-
-// const calendarSchema = new mongoose.Schema({
-//   events: [eventSchema],
-// });
-
 const calendarModel = mongoose.model('Calendar', calendarSchema);
 
 module.exports = calendarModel;

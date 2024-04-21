c4591b945fb0b2b7d466fed5e03391ffbcba7356
VirejDasani <dasanivirej@gmail.com>
2024-04-21 16:44:17 +0100

    feat: use routes to put date setter under admin/

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,14 @@
 import './App.css';
-// import CalendarView from './components/Calendar/CalendarView';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DateSetter from './components/TermDateSetter/DateSetter';
 
-// using npm big calendar app. Tried npm react scheduler component but this is better
 function App() {
-  //   const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="App">
-        {/* <h1>Sweat_v2</h1> */}
-
-        {/* <CalendarView /> */}
-        <DateSetter />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/admin/set-key-dates" element={<DateSetter />} />
+      </Routes>
+    </Router>
   );
 }
 
